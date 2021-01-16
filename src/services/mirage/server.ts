a527e558029cda6164fabd91d4c42efde89d81b0
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-17 00:43:41 +0500

    Setup Server
    1:- Created
    	a:- src\services\mirage\server.ts to define error handling response and setup server to define different models, factories and dummy apiUrl

---
+++ src/services/mirage/server.ts
@@ -0,0 +1,54 @@
+// Imports
+// Mirage Imports
+import { Server, Model, Factory, Response, belongsTo, hasMany } from "miragejs";
+
+// Function to catch error in case of Error Response
+export const handleErrors = (error: any, message = "An error ocurred") => {
+  return new Response(400, undefined, {
+    data: {
+      message,
+      isError: true,
+    },
+  });
+};
+
+// Setup Mock server to use data in the app
+export const setupServer = (env?: string): Server => {
+  return new Server({
+    environment: env ?? "production",
+
+    // models to define properties of different data to be stored in the database on mock server
+    // .extend() adds different properties to the perticular data. while many/belongs relations are defined here
+    models: {
+      entry: Model.extend({
+        diary: belongsTo(),
+      }),
+      diary: Model.extend({
+        entry: hasMany(),
+        user: belongsTo(),
+      }),
+      user: Model.extend({
+        diary: hasMany(),
+      }),
+    },
+
+    // Create blueprint of the user data to be stored factoreis is used yo do so
+    factories: {
+      user: Factory.extend({
+        username: "test",
+        password: "password",
+        email: "test@email.com",
+      }),
+    },
+
+    // seeding data with the mirage server
+    seeds: (server): any => {
+      server.create("user");
+    },
+
+    // Handle request here urlPrefix is dummy Api
+    routes(): void {
+      this.urlPrefix = "https://diaries.app";
+    },
+  });
+};
